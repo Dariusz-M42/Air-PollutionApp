bfd8ff8aa86b13f0542bbd12749d4f4d925bf209
Dariusz-M42 <dariusz.murawko@gmail.com>
2025-04-15 12:54:45 +0200

    Add files via upload

---
+++ Air-PollutionApp/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,10 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "Air-PollutionApp_autogen"
+  "CMakeFiles\\Air-PollutionApp_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\Air-PollutionApp_autogen.dir\\ParseCache.txt"
+  )
+endif()
